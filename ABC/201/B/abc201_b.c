729da36c96aa85d1d2c4be288593b6587eee655d
K H <lovetkg3584@gmail.com>
2022-02-27 00:13:59 +0900

    added abc201_b.c

--- ABC/201/B/abc201_b.c
+++ ABC/201/B/abc201_b.c
@@ -2,9 +2,30 @@
 // https://atcoder.jp/contests/abc201/tasks/abc201_b
 
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-    int n;
-    char** s[16];
+    int i, n;
+    char s[16];
+    int h;
+    char ranking_s[2][16] = {"init_1", "init_2"};
+    int ranking_h[2] = {0, 0};
     scanf("%d", &n);
+
+    for(i = 0; i < n; i++){
+        scanf("%s", s);
+        scanf("%d", &h);
+        if(ranking_h[0] < h){
+            strcpy(ranking_s[1], ranking_s[0]);
+            strcpy(ranking_s[0], s);
+            ranking_h[1] = ranking_h[0];
+            ranking_h[0] = h;
+        }else if(ranking_h[1] < h){
+            strcpy(ranking_s[1], s);
+            ranking_h[1] = h;
+        }
+    }
+
+    printf("%s", ranking_s[1]);
+    return 0;
 }
