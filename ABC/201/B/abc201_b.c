0eba77e1dd16c33fcda62aaa6c58f8fc1ad947cc
K H <lovetkg3584@gmail.com>
2022-02-26 23:52:50 +0900

    added abc201_b.c

---
+++ ABC/201/B/abc201_b.c
@@ -0,0 +1,10 @@
+// B - Do you know the second highest mountain?
+// https://atcoder.jp/contests/abc201/tasks/abc201_b
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    char** s[16];
+    scanf("%d", &n);
+}
